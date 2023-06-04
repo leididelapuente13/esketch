3b7b5ae24e51ac6b36d88a3b60deaf504ad93c3c
Leidi <leididelapuente3@gmail.com>
2023-06-04 01:33:32 -0500

    Edit: UI for big screens because there was a conflict. added: function to calc a proportional width and height for the grid items

--- script.js
+++ script.js
@@ -6,24 +6,28 @@ let btnBorder = document.querySelector(".opciones__border");
 let btnClear = document.querySelector(".opciones__clear");
 let tamGrid = document.querySelector(".opciones__tam-grid");
 let showTam = document.querySelector(".opciones__mostrar-tam");
-let gridContainer = document.querySelector(".container")
+let gridContainer = document.querySelector(".container");
 
 tamGrid.addEventListener('input', () => {
     const tam = tamGrid.value;
     showTam.textContent = `${tam} x ${tam}`;
     clearGrid();
     createGrid(tam);
-    gridContainer.style.gridTemplateColumns = `repeat(${tam}, 1fr`;
-    gridContainer.style.gridTemplateRows = `repeat(${tam}, 1fr)`;
+    gridContainer.style.gridTemplateColumns = `repeat(${tam}, auto)`;
+    gridContainer.style.gridTemplateRows = `repeat(${tam}, auto)`;
 });
 
 function createGrid(tam) {
     let gridContainer = document.querySelector(".container");
-    for (let i = 0; i < tam*tam; i++) {
+    const containerWidth = gridContainer.clientWidth;
+    const elementSize = containerWidth / tam;
+
+    for (let i = 0; i < tam * tam; i++) {
         let grid = document.createElement("div");
         grid.classList.add("item");
         gridContainer.appendChild(grid);
-        grid.innerHTML = `${i+1}`;
+        grid.style.width = `${elementSize}px`;
+        grid.style.height = `${elementSize}px`;
     }
 } 
 
